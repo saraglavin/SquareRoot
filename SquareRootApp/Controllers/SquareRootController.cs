bcdeed00dccee8fc92c6ab74173104b9284bd6f0
saraglavin <hej@saraglavin.se>
2019-01-29 14:05:38 +0100

    blabla

---
+++ SquareRootApp/Controllers/SquareRootController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SquareRootApp.Controllers
+{
+    [Route("api")] // Behövs om man har många metoder, tex api/squareroot orkar an inte skeiva på alla
+    public class SquareRootController : Controller
+    {
+        [HttpGet("SquareRoot")]
+        public IActionResult SquareRoot(int? number)
+        {
+          
+                return Ok(Math.Sqrt((int)number));
+
+                //double x = (int)number;
+                //var root = Math.Sqrt(x);
+
+                //return Ok(root);
+            
+
+        }
+      
+    }
+}
---
+++ SquareRootApp/wwwroot/JavaScript.js
@@ -0,0 +1,35 @@
+﻿async function squareRoot() {
+
+    let number = document.getElementById("number").value;
+    //let result = document.getElementById("result");
+    //result.innerText = `Du angav ${number}`;
+
+    document.getElementById("error").innerText = "";
+
+    if (isNaN(number)) {
+
+
+        document.getElementById("error").innerText = "Du måste ange ett nummer!";
+    }
+
+    else if (number < 0) {
+
+        document.getElementById("error").innerText = "Du kan inte ange ett negativt nummer!";
+
+    }
+
+
+    else {
+
+        let response = await fetch(`api/squareroot?number=${number}`);
+
+        if (response.status === 200) {
+            let result = await response.json();
+            document.getElementById("result").innerText = result;
+        }
+
+        else {
+
+        }
+    }
+}
